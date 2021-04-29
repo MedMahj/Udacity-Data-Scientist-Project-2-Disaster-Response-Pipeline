034a5bc29374b31b997d335cfe770b8ba9e6a14a
MedMahj <mahjoubm@hotmail.fr>
2021-04-29 15:42:26 +0200

    first commit

---
+++ models/train_classifier.py
@@ -0,0 +1,118 @@
+import time
+import datetime
+
+# download necessary NLTK data
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+# import libraries
+import re
+import sys
+import numpy as np
+import pandas as pd
+import pickle
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+
+
+def load_data(database_filepath):
+
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('Messages', engine)
+    X = df.message.values
+    Y = df.iloc[:,4:].values
+    category_names = df.columns[4:]
+
+    return X,Y,category_names
+    
+
+def tokenize(text):
+
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    # get list of all urls using regex
+    detected_urls = re.findall(url_regex, text)
+    
+    # replace each url in text string with placeholder
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
+
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    pass
+
+
+def save_model(model, model_filepath):
+
+    pickle.dump(model, open(model_filepath, 'wb'))
+   
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+
+        start = time.time()
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        end = time.time()
+        print('Model Trained in ', str(datetime.timedelta(seconds=(end-start))))
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
